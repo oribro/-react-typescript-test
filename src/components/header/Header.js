366cf323c7a9b1e757fc5306d239d981636d5b0e
Ori Broda <ori.broda@grovv.co.il>
2022-10-22 12:06:32 +0300

    Add Barcode Scanner

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './header.css';
+
+const Header = () => (
+  <header className="header">
+     <h1 className="header__title">Mekorot Barcode Scanner <span role="img" aria-label="water-drop">💦
+</span></h1>
+  </header>
+);
+
+export default Header;
